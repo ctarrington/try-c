711188bd3b4bc279d5440cbb70f8f0ac7e6d03f1
ctarrington <arrington.ct@gmail.com>
2020-02-12 08:57:28 -0500

    housekeeping and testing git

--- testing/GreetingTest.cpp
+++ testing/GreetingTest.cpp
@@ -1,12 +1,7 @@
 #include "gtest/gtest.h"
 #include "Greeting.h"
 
-
-TEST(SillyTests, Big) {
-    EXPECT_GT(3, 1);
-}
-
-TEST(SillyTests, Greet) {
+TEST(GreetingTests, Greet) {
     auto greeting = new Greeting("Hi");
     EXPECT_EQ(greeting->sayHelloTo("Joe"), "Hi, Joe");
 }
