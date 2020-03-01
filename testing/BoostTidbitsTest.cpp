b8593ce6468b758d054f2e7dd5fc375d880d6778
ctarrington <arrington.ct@gmail.com>
2020-03-01 09:23:46 -0500

    boost configuration

--- testing/BoostTidbitsTest.cpp
+++ testing/BoostTidbitsTest.cpp
@@ -1,10 +1,11 @@
+#include <iostream>
+
 #include "gtest/gtest.h"
 
 #include <boost/date_time/gregorian/gregorian.hpp>
 
-TEST(BoostDatesTests, simpleConstruction) {
+TEST(BoostTidbitsTests, simpleDateConstruction) {
     boost::gregorian::date d{ 1986, 9, 15 };
 
     EXPECT_EQ(1986, d.year());
 }
-
