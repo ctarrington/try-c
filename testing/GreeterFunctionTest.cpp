2d72039a6ce3a0fdaada9c6e60e0991981b7d0ed
ctarrington <arrington.ct@gmail.com>
2020-03-10 19:07:38 -0400

    playing with lambdas and boost

--- testing/GreeterFunctionTest.cpp
+++ testing/GreeterFunctionTest.cpp
@@ -1,15 +1,28 @@
 #include "gtest/gtest.h"
 
+#include <boost/format.hpp>
+
 using namespace std;
 
-TEST(GreeterFunctionTest, simple) {
+TEST(GreeterFunctionTest, oldSchool) {
     const char* greeting = "Hi";
-    auto greet = [greeting](const char* name, char* destinationBuffer) {
-        sprintf(destinationBuffer, "%s, %s", greeting, name);
+
+    auto greet = [greeting](const char* name, char* destinationBuffer, char endMark = '.') {
+        sprintf(destinationBuffer, "%s, %s%c", greeting, name, endMark);
         return destinationBuffer;
     };
 
     char sentence[50];
-    greet("Fred", sentence);
-    EXPECT_STREQ("Hi, Fred", greet("Fred", sentence));
+    EXPECT_STREQ("Hi, Fred.", greet("Fred", sentence));
+    EXPECT_STREQ("Hi, Fred!", greet("Fred", sentence, '!'));
 }
+
+
+TEST(GreeterFunctionTest, newSchool) {
+    boost::format format("Hi, %1%%2%");
+
+    string greeting = str(format % "Fred" % "!");
+    EXPECT_STREQ("Hi, Fred!", greeting.c_str());
+}
+
+
