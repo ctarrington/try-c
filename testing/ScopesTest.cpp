8f7fbf63f48639651cd6ee4661b65d9b46381074
ctarrington <arrington.ct@gmail.com>
2020-03-14 23:15:01 -0400

    scoping

---
+++ testing/ScopesTest.cpp
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+
+using namespace std;
+
+static int accessCounter = 0;
+
+int collect(int value) {
+    static int accumulated = 0;
+
+    accumulated += value;
+    accessCounter++;
+    return accumulated;
+}
+
+TEST(ScopesTest, static) {
+    int c = collect(10);
+    EXPECT_EQ(10, c);
+
+    c = 1000;
+    EXPECT_EQ(1000, c);
+
+    c = collect(10);
+    EXPECT_EQ(20, c);
+
+    EXPECT_EQ(2, accessCounter);
+}
